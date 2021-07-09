5fc1e828ee2be8125f4b3334959d964223727c83
Gerson <hidsegiu@gmail.com>
2021-07-09 02:26:54 -0500

    34. Create Book and Validations

--- BookListRazor/Model/Book.cs
+++ BookListRazor/Model/Book.cs
@@ -10,7 +10,9 @@ namespace BookListRazor.Model
     {
         [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public string Author { get; set; }
+        public string ISBN { get; set; }
     }
 }
